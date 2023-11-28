a5f85efce4089a4ba79a45d0b29c6224769953c5
amanullahjn99 <amanullahjn99@gmail.com>
2023-11-27 22:20:37 -0500

    Nov27

---
+++ src/main/java/review/E3.java
@@ -0,0 +1,13 @@
+package review;
+
+public class E3 {
+    void setData(){
+        String name="Ehab";
+    }
+    void getData(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
